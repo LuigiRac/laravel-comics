deaec7a0e42ec81676774d998342ad03d833179e
LuigiRac <lui.rac06@gmail.com>
2025-04-28 20:04:42 +0200

    footer

--- laravel-comics/resources/views/partials/footer.blade.php
+++ laravel-comics/resources/views/partials/footer.blade.php
@@ -1,3 +1,17 @@
-<footer>
-    <h2>FOOTER</h2>
+
+ <footer>
+        <div class="footer-container">
+            <div class="left-button">
+                <button class="signup-button">SIGN-UP NOW!</button>
+            </div>
+
+            <div class="right-section">
+                <a href="#"><img src={{ Vite::asset ('resources/img/footer-facebook.png')}} alt="Facebook"></a>
+                <a href="#"><img src={{ Vite::asset ('resources/img/footer-twitter.png')}} alt="Twitter"></a>
+                <a href="#"><img src={{ Vite::asset ('resources/img/footer-youtube.png')}} alt="Youtube"></a>
+                <a href="#"><img src={{ Vite::asset ('resources/img/footer-pinterest.png')}} alt="Pinterest"></a>
+                <a href="#"><img src={{ Vite::asset ('resources/img/footer-periscope.png')}} alt="Periscope"></a>
+                </div>
+        </div>
 </footer>
+
